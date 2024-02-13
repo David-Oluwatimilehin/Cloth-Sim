625c910c1d1608e4478e35def07ed84814757134
David O <124046476+David-Soremekun@users.noreply.github.com>
2024-02-13 10:34:30 +0000

    Spawn Particles

--- Assets/Script/Spawner.cs
+++ Assets/Script/Spawner.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class Spawner : MonoBehaviour
 {
@@ -15,8 +16,85 @@ public class Spawner : MonoBehaviour
     //public GameObject prefab;
     List<Particle> particles;
     public GameObject prefab;
-    
-    
+    public IObjectPool<Particle> objectPool;
+
+    public enum PoolType
+    {
+        Stack,
+        LinkedList
+    }
+
+    public PoolType poolType;
+
+    // Collection checks will throw errors if we try to release an item that is already in the pool.
+    public bool collectionChecks = true;
+    public int maxPoolSize = 25;
+
+    IObjectPool<Particle> m_Pool;
+
+    public IObjectPool<Particle> Pool
+    {
+        get
+        {
+            if (m_Pool == null)
+            {
+                if (poolType == PoolType.Stack)
+                    m_Pool = new ObjectPool<Particle>(CreatePooledItem, OnTakeFromPool, OnReturnedToPool, OnDestroyPoolObject, collectionChecks, 25, maxPoolSize);
+                else
+                    m_Pool = new LinkedPool<Particle>(CreatePooledItem, OnTakeFromPool, OnReturnedToPool, OnDestroyPoolObject, collectionChecks, maxPoolSize);
+            }
+
+            return m_Pool;
+        }
+    }
+    void OnReturnedToPool(Particle system)
+    {
+        system.gameObject.SetActive(false);
+    }
+
+    // Called when an item is taken from the pool using Get
+    void OnTakeFromPool(Particle system)
+    {
+        system.gameObject.SetActive(true);
+    }
+
+    // If the pool capacity is reached then any items returned will be destroyed.
+    // We can control what the destroy behavior does, here we destroy the GameObject.
+    void OnDestroyPoolObject(Particle system)
+    {
+        Destroy(system.gameObject);
+    }
+    Particle CreatePooledItem()
+    {
+        var go = new GameObject("Pooled Object");
+        go.transform.position = transform.position * Random.insideUnitCircle * fireSpawnRadius;
+        
+        Particle ps = go.AddComponent<Particle>();
+        
+        
+
+        // This is used to return ParticleSystems to the pool when they have stopped.
+        var returnToPool = go.AddComponent<Spawner>();
+        returnToPool.objectPool = Pool;
+
+        return ps;
+    }
+    void OnGUI()
+    {
+        GUILayout.Label("Pool size: " + Pool.CountInactive);
+        if (GUILayout.Button("Create Particles"))
+        {
+            
+            for (int i = 0; i < NumberOfParticles; ++i)
+            {
+                //var ps = Pool.Get();
+                //ps.InitialiseParticles(transform.position, MaximumSize, MinnimumSize, initialLifeTime);
+                //ps.transform.SetParent(transform);
+
+            }
+        }
+    }
+
     private void SpawnParticles()
     {
         GameObject gameObject = Instantiate(prefab);
@@ -62,13 +140,15 @@ public class Spawner : MonoBehaviour
     }
     void UpdateParticleLifetime()
     {
+        var itemsToAdd = new List<string>();
+
         foreach (Particle p in particles )
         {
             p.lifeTime -= Time.deltaTime;
 
             if (p.lifeTime <= 0f)
             {
-                // Recycle or respawn the particle
+                
                 SpawnParticles();
             }
         }
@@ -87,7 +167,13 @@ public class Spawner : MonoBehaviour
     void Start()
     {
         particles = new List<Particle>();
+
         
+
+        
+
+
+
         SpawnParticles();
     }
     void RemoveExpiredParticles()
@@ -115,9 +201,9 @@ public class Spawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        UpdateParticleVisuals();
-        UpdateParticleMovement();
-        UpdateParticleLifetime();
+        //UpdateParticleVisuals();
+        //UpdateParticleMovement();
+        //UpdateParticleLifetime();
         //RemoveExpiredParticles();
     }
 }
