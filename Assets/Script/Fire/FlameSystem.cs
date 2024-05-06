154cff7861ab9ccc6349c2f282de69b078f93036
David S <b1053474@live.tees.ac.uk>
2024-05-06 18:19:34 +0100

    Commit

--- Assets/Script/Fire/FlameSystem.cs
+++ Assets/Script/Fire/FlameSystem.cs
@@ -26,8 +26,8 @@ public class FlameSystem : MonoBehaviour
     public float particleSize;
     public int numberOfParticles;
 
-    public float emissionRate = 50;
-    private float nextEmissionTime = 0.0f;
+    public float emissionRate = 10;
+    public float nextEmissionTime = 0.0f;
 
     public List<FlameParticle> particleList;
     public GameObject particlePrefab;
@@ -35,23 +35,25 @@ public class FlameSystem : MonoBehaviour
     public int nrAlive;
     public Gradient gradient;
     public int maxNum;
+
     private Vector3 gravity;
+    private Vector3 velocity;
+    private Vector3 normal;
     private float sizeRatio;
     
     void Start()
     {
         particleList = new List<FlameParticle>();
-        gravity = new Vector3(0, 0, 0);
-        ParticleInitialisation();
+        gravity = new Vector3(0, 0.981f, 0);
         
-        
+        ParticleInitialisation();       
     }
     public Vector3 ComputeVelocity()
     {
-        Vector3 velocity = Vector3.zero;
-        Vector3 normal = spawnArea.position.normalized;
+        velocity = Vector3.zero;
+        normal = spawnArea.position.normalized;
         
-        velocity = new Vector3(Random.Range(-1f, 1f), Random.Range(minSpeed, maxSpeed), 0);
+        velocity = new Vector3(Random.Range(-1f, 1f), Random.Range(minSpeed, maxSpeed), 0/*Random.Range(-0.5f, 0.5f)*/);
         velocity *= normal.magnitude;
 
         velocity += gravity;
@@ -68,13 +70,15 @@ public class FlameSystem : MonoBehaviour
             spawnSphere = spawnArea.position;
             spawnSphere += Random.insideUnitSphere * spawnRadius;
             
-            particlePrefab = Instantiate(particlePrefab, new Vector3(spawnSphere.x, spawnSphere.y, spawnSphere.z), Quaternion.identity);
+            particlePrefab = Instantiate(particlePrefab, spawnSphere, Quaternion.identity);
             particlePrefab.name = "Particle: " + (i + 1);
             particlePrefab.transform.SetParent(spawnArea, false);
 
             particlePrefab.GetComponent<FlameParticle>().isEnabled = false;
             particlePrefab.GetComponent<FlameParticle>().lifetime = particleLifetime;
             particlePrefab.GetComponent<FlameParticle>().size = particleSize;
+            particlePrefab.GetComponent<FlameParticle>().pos = Vector3.zero;
+            particlePrefab.GetComponent<FlameParticle>().vel = Vector3.zero;
 
             particleList.Add(particlePrefab.GetComponent<FlameParticle>());
             
@@ -91,9 +95,10 @@ public class FlameSystem : MonoBehaviour
     private void ResetParticlePositions(FlameParticle particle)
     {
         particle.lifetime = particleLifetime;
-        particle.size = particleSize;
-        particle.pos = spawnArea.position + spawnRadius * Random.insideUnitSphere;
-        particle.vel = ComputeVelocity();
+        //particle.size = particleSize;
+        //particle.pos = spawnArea.position + spawnRadius * Random.insideUnitSphere;
+        //particle.vel = ComputeVelocity();
+        particle.isEnabled = false;
         
     }
 
@@ -105,28 +110,30 @@ public class FlameSystem : MonoBehaviour
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(spawnArea.position, spawnRadius);
 
-        if (!particleList.IsUnityNull())
+
+        foreach (var particle in particleList)
         {
-            
-            foreach (var particle in particleList)
+            Gizmos.color = particle.colour;
+            if (particle.isEnabled)
             {
-                Gizmos.color = particle.colour;
-                if (particle.isEnabled)
-                {
-                    Gizmos.DrawSphere(particle.pos,particle.size);
-                }
+                Gizmos.DrawSphere(particle.pos, particle.size);
             }
         }
-
-        
 
     }
     void Update()
     {
-        while (Time.time >= nextEmissionTime)
+        //while (Time.time >= nextEmissionTime)
+        //{
+            
+        //     EmitParticles();
+            
+        //    nextEmissionTime += 1f / emissionRate; // Increment next emission time
+        //}
+        if (Time.time >= nextEmissionTime)
         {
             EmitParticles();
-            nextEmissionTime += 1f / emissionRate; // Increment next emission time
+            nextEmissionTime = Time.time + 1f / emissionRate; // Calculate next emission time
         }
 
         foreach (var particle in particleList)
@@ -136,8 +143,8 @@ public class FlameSystem : MonoBehaviour
                 particle.lifetime -= Time.deltaTime;
                 sizeRatio = particle.lifetime / particleLifetime;
 
-                particle.size = particle.size * sizeRatio;
-                particle.colour = gradient.Evaluate(1 - particle.lifetime);
+                particle.size = Mathf.Clamp(particle.size, particle.size * sizeRatio, 0.05f);
+                particle.colour = gradient.Evaluate(particleLifetime - particle.lifetime);
 
             }
         }
@@ -154,30 +161,55 @@ public class FlameSystem : MonoBehaviour
             }
             else 
             {
+                if (flameParticle.isEnabled)
+                {
+                    flameParticle.pos += flameParticle.vel * Time.fixedDeltaTime;
+                }
                 
-                flameParticle.pos += flameParticle.vel * Time.fixedDeltaTime;
-
             }
         }
     }
 
     private void EmitParticles()
     {
-        Debug.Log("Function Called");
-        for(int i=0; i<particleList.Count; i++)
+        for(int i = 0; i<particleList.Count; i++)
         {
             
-            if (!particleList[i].enabled)
+            if (!particleList[i].isEnabled)
             {
-
+                
+                particleList[i].isEnabled = true;
                 particleList[i].lifetime = particleLifetime;
                 particleList[i].size = particleSize;
-                particleList[i].pos = spawnArea.position + Random.insideUnitSphere * spawnRadius;
+
+                //Debug.LogError("Function Called");
+
+                particleList[i].pos = spawnArea.position + spawnRadius * Random.insideUnitSphere;
                 particleList[i].vel = ComputeVelocity();
-                particleList[i].enabled = true;
+
                 nrAlive++;
+
             }
+            
+
         }
+        //for(int i=0; i<particleList.Count; i++)
+        //{
+        //    //Debug.Log("Function Called " + i + " time");
+        //    for(int j=0; j<numberOfParticles; j++)
+        //    {
+        //        if (!particleList[i].enabled)
+        //        {
+        //            particleList[i].enabled = true;
+        //            particleList[i].lifetime = particleLifetime;
+        //            particleList[i].size = particleSize;
+        //            particleList[i].pos = spawnArea.position + Random.insideUnitSphere * spawnRadius;
+        //            particleList[i].vel = ComputeVelocity();
+                    
+        //            nrAlive++;
+        //        }
+        //    }
+        //}
     }
 
     void LateUpdate()
